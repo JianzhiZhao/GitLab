09d34fa850bf5c3af39496eba5801ce3adcbaf19
JianzhiZhao <34952367+JianzhiZhao@users.noreply.github.com>
2021-02-26 11:34:38 +0800

    Add files via upload

---
+++ GestureController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Jianzhi.Controller
+{
+    public class GestureController : MonoBehaviour
+    {
+        public Image controllableImg;
+
+        private Vector3? lastTouchPosition = null;
+
+        
+
+        private void Update() 
+        {
+            if(Input.touchCount == 1)
+            {
+                Touch current_Touch = Input.GetTouch(0);
+                controllableImg.transform.Translate(current_Touch.deltaPosition);
+                
+                float x = controllableImg.rectTransform.position.x;
+                float width = controllableImg.rectTransform.sizeDelta.x;
+                x = Mathf.Max(x, - width / 2f);
+                x = Mathf.Min(x, Screen.width + controllableImg.preferredWidth / 2f);
+
+                float y = controllableImg.rectTransform.position.y;
+                y = Mathf.Max(y, - controllableImg.preferredHeight / 2f);
+                y = Mathf.Min(y, Screen.height + controllableImg.preferredHeight / 2f);
+
+                controllableImg.rectTransform.position = new Vector2(x,y);
+            }
+        }
+
+        public void Test()
+        {
+            Debug.Log("Image:"+controllableImg.rectTransform.position+" ("+controllableImg.preferredWidth+","+controllableImg.preferredHeight+")");
+            Debug.Log("Screen:("+Screen.width+","+Screen.height+")");
+        }
+    }
+}
+
